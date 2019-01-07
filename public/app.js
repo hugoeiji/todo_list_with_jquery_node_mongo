7286d90313e41d44a194519a99928f3bd1016ca9
Hugo Carvalho <hugo@hugo.com>
2019-01-07 21:50:09 +0000

    extra bits of code, now the js api frontend is ready

--- public/app.js
+++ public/app.js
@@ -3,12 +3,77 @@
 $(document).ready(function(){
     $.getJSON("/api/todos")
     .then(addTodos)
+    
+    $("#todoInput").keypress(function(event){
+        if(event.which == 13) {
+            createTodo();
+        }
+    });
+    
+    $(".list").on("click", "li", function(){
+        updateTodo($(this))
+    })
+    
+    $(".list").on("click", "span", function(event){
+        event.stopPropagation();
+        removeTodo($(this).parent());
+    })
+    
 });
+
 
 function addTodos(todos) {
     // Add todos to the page
     todos.forEach(function(todo){
-        var newTodo = $('<li class="task">' + todo.name + "</li>");
-        $(".list").append(newTodo);
+        addTodo(todo);
+    })
+}
+
+function addTodo(todo) {
+    var newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>');
+    newTodo.data('id', todo._id);
+    newTodo.data('completed', todo.completed);
+    if(todo.completed){
+        newTodo.addClass("done");
+    };
+    $(".list").append(newTodo);
+}
+
+function createTodo() {
+    var userInput = $("#todoInput").val();
+    $.post("/api/todos", {name: userInput})
+    .then(function(newTodo){
+        addTodo(newTodo);
+        $("#todoInput").val('');
+    })
+    .catch(function(err){
+        console.log(err);
+    })
+}
+
+function removeTodo(todo){
+    var clickedId = todo.data('id');
+    var deleteUrl = "api/todos/" + clickedId;
+    $.ajax({
+        method: "DELETE",
+        url: deleteUrl
+    })
+    .then(function(data){
+        todo.remove();
+    })
+}
+
+function updateTodo(todo){
+    var updateUrl = "/api/todos/" + todo.data("id");
+    var isDone = !todo.data('completed');
+    var updateData = {completed: isDone};
+    $.ajax({
+        method: "PUT",
+        url: updateUrl,
+        data: updateData
+    })
+    .then(function(updatedTodo){
+        todo.toggleClass("done");
+        todo.data('completed', isDone);
     })
 }
