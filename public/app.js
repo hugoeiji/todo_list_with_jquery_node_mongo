bca76b46b6b092907074c61f34bc5842a95214e8
EC2 Default User <ec2-user@ip-172-31-37-109.ec2.internal>
2019-01-07 04:52:35 +0000

    base project

---
+++ public/app.js
@@ -0,0 +1,14 @@
+/* global $ */
+
+$(document).ready(function(){
+    $.getJSON("/api/todos")
+    .then(addTodos)
+});
+
+function addTodos(todos) {
+    // Add todos to the page
+    todos.forEach(function(todo){
+        var newTodo = $('<li class="task">' + todo.name + "</li>");
+        $(".list").append(newTodo);
+    })
+}
